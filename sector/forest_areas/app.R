569cc80e1777c1da4188cc2fe1e060039d0fcc82
iliastsergoulas <htsergoulas@yahoo.gr>
2017-05-11 13:19:22 +0300

    Adding forest villages

---
+++ sector/forest_areas/app.R
@@ -0,0 +1,57 @@
+# Data: Eggs plants in Greece
+# This R script is created as a Shiny application to process data 
+# from Greek Ministry of Agriculture and create plots and maps.
+# The code is available under MIT license, as stipulated in https://github.com/iliastsergoulas/shinyapps/blob/master/LICENSE.
+# Author: Ilias Tsergoulas, Website: www.agristats.eu
+
+library(shiny)
+library(shinythemes)
+library(leaflet)
+library(rgdal)
+library(maptools)
+library(htmltools)
+library(shinydashboard)
+
+plants <- readShapePoly("/home/iliastsergoulas/Desktop/test.shp")
+
+header <- dashboardHeader(title = "Μονάδες τυποποίησης και συσκευασίας αυγών", titleWidth=500) # Header of dashboard
+sidebar <- dashboardSidebar(disable = TRUE)# Disabling sidebar of dashboard
+frow1 <- fluidRow( # Creating row of valueboxes
+    leafletOutput("map_points")
+)
+body <- dashboardBody(frow1) # Binding rows to body of dashboard
+ui <- dashboardPage(header, sidebar, body, skin="green") # Binding elements of dashboard
+
+server <- function(input, output, session) {
+    output$map_points <- renderLeaflet({
+        leaflet() %>%
+            addProviderTiles("OpenStreetMap.Mapnik",
+                             options = providerTileOptions(noWrap = TRUE)
+            ) %>%
+            addPolygons(data = plants)
+    })
+    output$regions<-renderPlot({ # Per region
+        ggplot(plants_per_region, aes(x = factor(region_nam))) + 
+            geom_bar(stat="count", fill="steelblue",width=0.5, color="steelblue") + 
+            xlab("Περιφέρεια") + ylab("Αριθμός μονάδων") + 
+            theme(axis.text.x=element_text(angle=90, hjust=1)) + 
+            geom_text(stat='count',aes(label=..count..),vjust=-1) + 
+            theme(legend.title=element_blank()) + 
+            theme(plot.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=20)) +
+            theme(axis.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=14)
+        )  
+    })
+    output$prefectures<-renderPlot({ # Per prefecture
+        ggplot(plants_per_pref, aes(x = factor(prefecture))) + 
+            geom_bar(stat="count", fill="steelblue") + 
+            xlab("Περιφερειακή ενότητα") + ylab("Αριθμός μονάδων") + 
+            theme(axis.text.x=element_text(angle=90, hjust=1)) + 
+            geom_text(stat='count',aes(label=..count..),vjust=-1) + 
+            theme(legend.title=element_blank()) + 
+            theme(plot.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=20)) +
+            theme(axis.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=14)
+        )  
+    })
+}
+
+shinyApp(ui, server)
