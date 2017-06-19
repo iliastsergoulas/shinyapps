48cdaca93245e0456a7b32e5170ef59abf187b9d
iliastsergoulas <htsergoulas@yahoo.gr>
2017-06-19 22:19:04 +0300

    New scripts

--- sector/en/apiculture_centres/app.R
+++ sector/en/apiculture_centres/app.R
@@ -57,6 +57,7 @@ server <- function(input, output, session) {
                        popup = ~htmlEscape(name))
     })
     output$list <- renderDataTable({ # Creating summary by country
+        plants_list<-plants_list[c("name", "address", "telephone", "e-mail", "fax")]
         plants_list}, options = list(lengthMenu = c(5, 25, 50), pageLength = 10)
     )
 }
