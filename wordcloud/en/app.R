d0332b9eb0c9ea15e38f5af40167ce343459479d
iliastsergoulas <htsergoulas@yahoo.gr>
2017-07-29 15:15:51 +0300

    Changed wordcloud

--- wordcloud/en/app.R
+++ wordcloud/en/app.R
@@ -27,17 +27,20 @@ myCorpus <- Corpus(VectorSource(mydata$`0`)) # Building a corpus
 # Creating matrix od tweets after "cleaning" them from anything unnecessary
 myDtm <- TermDocumentMatrix(myCorpus, control = 
             list(removePunctuation = TRUE, 
-                 stopwords = c("agronewsgr", "https", "amp", "food", "read", "https","without",
-                               "thanks", "look", "looking", "see", "people", "way","fms","hemp",
-                               "gaid", "agchattruth", "foodtruth","agchat","want","list","make",
-                               "agriculture","will", "talk", "now", "new", "forget","like","using",
+                 stopwords = c("agronewsgr", "https", "amp", "food", "read", "https","without","bring",
+                               "thanks", "look", "looking", "see", "people", "way","fms","hemp","due",
+                               "gaid", "agchattruth", "foodtruth","agchat","want","list","make","range",
+                               "agriculture","will", "talk", "now", "new", "forget","like","using","ghg",
                                "farm", "thank", "book", "year", "week", "photo","today","farmers",
                                "add", "can", "truths", "life", "news", "every","lelyknowhow","farming",
                                "time", "action", "know", "share", "just", "found","always","day","farms",
                                "latest", "find", "get", "great", "table", "join", "word", "countries",
                                "thunderclap", "excited", "better", "voice", "video","use","asa","ahdb",
-                               "paper", "getting", "blogs", "help", "at ...", "...", "one","unfao","via",
-                               "best", "animal", "need", "joy", "many", "awesome", "support", 
+                               "paper", "getting", "blogs", "help", "one","unfao","via","...","_",
+                               "best", "animal", "need", "joy", "many", "awesome", "support", "first",
+                               "futureofcap","sacfarmclimate","offers","shows","play","-","means","hort",
+                               "allow","provides","nfutweets","never","century","address","say","since",
+                               "almost","philhoganeu","farmwildlifeuk","min","nyc","around","devex","xhnews",
                                "review", stopwords("english")),
                  removeNumbers = TRUE, tolower = TRUE))
 m <- as.matrix(myDtm) # Converting to matrix
@@ -50,7 +53,7 @@ ui <- fluidPage( # Creating shiny app's UI
 )
 server <- function(input, output) {
     output$view <- renderPlot({ # Creating wordcloud
-        wordcloud(d$word, d$freq, min.freq=2, max.words=50, scale=c(3,.5), 
+        wordcloud(d$word, d$freq, min.freq=3, max.words=100, scale=c(10,.5), 
                   rot.per=.5,colors=brewer.pal(8, "Dark2"))
     })
 }
